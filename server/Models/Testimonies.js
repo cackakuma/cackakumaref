222c5a5665351aa0dc6312d76760f0470cd4b6e1
cackakuma <cackakuma@gmail.com>
2025-11-24 15:53:30 +0200

    Delete server directory

--- server/Models/Testimonies.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const testSchema = new mongoose.Schema({
-      fullName: String,
-      email: String,
-      message: String
-});
-
-module.exports = mongoose.model("Testimony", testSchema);
