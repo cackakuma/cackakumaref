0be6626093f0fb2db57a1c16997dc7051431b444
cackakuma <cackakuma@gmail.com>
2025-12-04 16:37:08 +0200

    Update loadingScreen.jsx

--- client/src/components/loadingScreen.jsx
+++ client/src/components/loadingScreen.jsx
@@ -1,6 +1,6 @@
 const LoadingScreen = () => {
   return (
-    <div className="absolute inset-0 flex items-center justify-center rounded-2xl z-50 backdrop-blur-sm pointer-events-none">
+    <div className="absolute inset-0 flex items-center justify-center rounded-2xl z-50 backdrop-blur-sm pointer-events-none min-h-screen">
       <div className="animate-spin rounded-full h-14 w-14 border-t-4 border-b-4 border-blue-600"></div>
     </div>
   );
