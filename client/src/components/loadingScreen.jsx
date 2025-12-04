6102a3ef1a2568f280d26ac5737d82f42cf04a20
cackakuma <cackakuma@gmail.com>
2025-12-04 16:24:58 +0200

    Update loadingScreen.jsx

--- client/src/components/loadingScreen.jsx
+++ client/src/components/loadingScreen.jsx
@@ -1,6 +1,6 @@
 const LoadingScreen = () => {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-[9999]">
+    <div className="fixed top-20 inset-0 bg-white bg-opacity-40 flex items-center justify-center z-[9999]">
       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-600"></div>
     </div>
   );
